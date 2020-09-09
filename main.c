0a2dbc2a0b83d0b382981e735fc6eb0a6967f7cd
Zachery Littell <zlittell@gmail.com>
2020-09-09 12:30:46 -0500

    CPU clocked from 48MHZ, timer interrupt 10mS from 32khz internal

--- main.c
+++ main.c
@@ -24,40 +24,42 @@
 
 void configureClocks(void)
 {
-    /* Configure 8MHz Clock
-     * Keep all flash read values for calibration, etc
-     * Set Prescaler to 3 (description 8)
-     * Set OnDemand to 1 (peripheral access during sleep turns on clock)
-     * Set RunStandby to 0 (clock doesn't run in sleep)
-     * Enable Clock
+    /* 
+     * Configure 8MHz Clock
      */
-    SYSCTRL_REGS->SYSCTRL_OSC8M = 
-            ((SYSCTRL_REGS->SYSCTRL_OSC8M & (SYSCTRL_OSC8M_FRANGE_Msk | SYSCTRL_OSC8M_CALIB_Msk)) |
-            SYSCTRL_OSC8M_PRESC(3) | SYSCTRL_OSC8M_ONDEMAND(1) | SYSCTRL_OSC8M_RUNSTDBY(0) | 
+    SYSCTRL_REGS->SYSCTRL_OSC8M |=
+            (SYSCTRL_OSC8M_PRESC(3) | SYSCTRL_OSC8M_ONDEMAND(1) | SYSCTRL_OSC8M_RUNSTDBY(0) | 
             SYSCTRL_OSC8M_ENABLE(1));
     while(!(SYSCTRL_REGS->SYSCTRL_PCLKSR & SYSCTRL_PCLKSR_OSC8MRDY(1))); //Wait for steady
     
-    GCLK_REGS->GCLK_GENDIV = (GCLK_GENDIV_DIV(0) | GCLK_GENDIV_ID(0));
-    GCLK_REGS->GCLK_GENCTRL = (GCLK_GENCTRL_GENEN(1) | GCLK_GENCTRL_SRC_OSC8M | GCLK_GENCTRL_ID(0));
+    GCLK_REGS->GCLK_GENDIV = (GCLK_GENDIV_DIV(0) | GCLK_GENDIV_ID(1));
+    GCLK_REGS->GCLK_GENCTRL = (GCLK_GENCTRL_GENEN(1) | GCLK_GENCTRL_SRC_OSC8M | GCLK_GENCTRL_ID(1));
     
-    /* Configure DFLL 48MHz
-     * Wait until DFLL is locked to output clock
-     * Coarse lock is disabled
-     * Quick Lock is Enabled
-     * Chill Cycle is enabled
-     * Only run clock when requested by peripheral
-     * Enable USB Clock Recovery Mode
-     * Locks will not be lost after waking up from sleep modes.
-     * Fine calibration tracks changes in output frequency
-     * DFLL Operates in open-loop mode
-     * DFLL Is Enabled
+    /* 
+     * Configure DFLL 48MHz
      */
     SYSCTRL_REGS->SYSCTRL_DFLLCTRL = 
             (SYSCTRL_DFLLCTRL_WAITLOCK(0) | SYSCTRL_DFLLCTRL_BPLCKC(0) | 
             SYSCTRL_DFLLCTRL_QLDIS(0) | SYSCTRL_DFLLCTRL_CCDIS(0) | 
-            SYSCTRL_DFLLCTRL_ONDEMAND(1) | SYSCTRL_DFLLCTRL_USBCRM(1) | 
+            SYSCTRL_DFLLCTRL_ONDEMAND(0) | SYSCTRL_DFLLCTRL_USBCRM(1) | 
             SYSCTRL_DFLLCTRL_LLAW(0) | SYSCTRL_DFLLCTRL_STABLE(0) | 
             SYSCTRL_DFLLCTRL_MODE(0) | SYSCTRL_DFLLCTRL_ENABLE(1));
+    
+    GCLK_REGS->GCLK_GENCTRL = (GCLK_GENCTRL_GENEN(1) | GCLK_GENCTRL_SRC_DFLL48M | GCLK_GENCTRL_ID(2));
+    
+    /*
+     * Set Clock Gen 0 to be 48MHz
+     * Clock Gen 0 Clocks the CPU
+     */
+    GCLK_REGS->GCLK_GENDIV = (GCLK_GENDIV_DIV(0) | GCLK_GENDIV_ID(0));
+    GCLK_REGS->GCLK_GENCTRL = (GCLK_GENCTRL_GENEN(1) | GCLK_GENCTRL_SRC_DFLL48M | GCLK_GENCTRL_ID(0));
+    
+    /*
+     * Configure 32.768KHZ internal oscillator on GCLK Channel 3 
+     */
+    SYSCTRL_REGS->SYSCTRL_OSC32K |= (SYSCTRL_OSC32K_ENABLE(1) | SYSCTRL_OSC32K_EN32K(1)) ;
+    GCLK_REGS->GCLK_GENDIV = (GCLK_GENDIV_DIV(0) | GCLK_GENDIV_ID(3));
+    GCLK_REGS->GCLK_GENCTRL = (GCLK_GENCTRL_GENEN(1) | GCLK_GENCTRL_SRC_OSC32K | GCLK_GENCTRL_ID(3));
 }
 
 void init_TC2(void)
@@ -65,18 +67,19 @@ void init_TC2(void)
     //Enable TC2 Bus Clock
     PM_REGS->PM_APBCMASK |= PM_APBCMASK_TC2(1);
     
-    // Set TC1/2 Generic Clock to GEN0 (8MHz)
+    // Set up Clock Gen
     GCLK_REGS->GCLK_CLKCTRL = 
             (GCLK_CLKCTRL_WRTLOCK(0) | GCLK_CLKCTRL_CLKEN(1) | 
-            GCLK_CLKCTRL_GEN_GCLK0| GCLK_CLKCTRL_ID_TC1_TC2);
+            GCLK_CLKCTRL_GEN_GCLK3 | GCLK_CLKCTRL_ID_TC1_TC2);
         
-    TC2_REGS->COUNT16.TC_INTENSET = (TC_INTENSET_ERR(1) | TC_INTENSET_MC1(1) | TC_INTENSET_MC0(1));
+    TC2_REGS->COUNT16.TC_INTENSET = (TC_INTENSET_ERR(1) | TC_INTENSET_MC0(1));
 
-    TC2_REGS->COUNT16.TC_CC[0] = 25;
+    //This should be ~10mS interrupts
+    TC2_REGS->COUNT16.TC_CC[0] = 328;
     
     TC2_REGS->COUNT16.TC_CTRLA = 
             (TC_CTRLA_PRESCSYNC_PRESC | TC_CTRLA_RUNSTDBY(0) | 
-            TC_CTRLA_PRESCALER_DIV1024 | TC_CTRLA_WAVEGEN_NFRQ | 
+            TC_CTRLA_PRESCALER_DIV1 | TC_CTRLA_WAVEGEN_NFRQ | 
             TC_CTRLA_MODE_COUNT16 | TC_CTRLA_ENABLE(1) | 
             TC_CTRLA_SWRST(0));
 }
