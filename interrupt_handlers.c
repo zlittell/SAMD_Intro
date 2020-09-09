da822a5e25ee20abd5d8090a0add928cfa6c770d
Zack Littell <zlittell@gmail.com>
2020-09-08 22:34:03 -0500

    timer is working. needed to reset count

--- interrupt_handlers.c
+++ interrupt_handlers.c
@@ -4,15 +4,16 @@
 void TC2_Handler()
 {
     //If TC2 OverFlow
-    if(TC2_REGS->COUNT32.TC_INTFLAG & TC_INTFLAG_OVF(1))
+    if(TC2_REGS->COUNT16.TC_INTFLAG & TC_INTFLAG_MC0(1))
     {
         //Clear interrupt
-        TC2_REGS->COUNT32.TC_INTFLAG = TC_INTFLAG_OVF(1);
+        TC2_REGS->COUNT16.TC_INTFLAG = TC_INTFLAG_MC0(1);
+        
+        TC2_REGS->COUNT16.TC_COUNT = 0;
         
         //Toggle LED
         PORT_REGS->GROUP[0].PORT_OUTTGL = PORT_PA16;
     }
-    
 }
 
 void enable_interrupts(void)
